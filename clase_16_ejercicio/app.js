a10a33b0177d76b211ef1c12f3f391bcca62d9fa
DanielFernandez14 <daniel.mfernandez1999@gmail.com>
2025-01-20 21:16:10 -0300

    Clase_16_Ejercicio

--- clase_16_ejercicio/app.js
+++ clase_16_ejercicio/app.js
@@ -3,14 +3,13 @@ const velocidad = document.querySelector("#velocidad")
 const distancia = document.querySelector("#distancia")
 const resultado = document.querySelector("h2")
 
-
 function calcularTiempo() {
-    const tiempo = distancia.value / velocidad.value
-    return true
+const tiempo = distancia.value / velocidad.value
+return Math.floor(tiempo)
 }
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const tiempoCalculado = calcularTiempo()
-    resultado.textContent = "El viaje tarda: " + tiempoCalculado + "hs."
+e.preventDefault()
+const tiempoCalculado = calcularTiempo()
+resultado.textContent = "El viaje tardara: " + tiempoCalculado + "hs"
 })
